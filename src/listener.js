5bae76f05c4bba7c1e1c6fdba93b4b60364ec211
Alfathan <alfathandp@gmail.com>
2022-10-04 19:17:35 +0700

    consumer program finished

---
+++ src/listener.js
@@ -0,0 +1,23 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+class listener {
+  constructor(notesService, mailSender) {
+    this._notesService = notesService;
+    this._mailSender = mailSender;
+
+    this.listen = this.listen.bind(this);
+  }
+
+  async listen(message) {
+    try {
+      const {userId, targetEmail} = JSON.parse(message.content.toString());
+      const notes = await this._notesService.getNotes(userId);
+      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(notes));
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+module.exports = listener;
